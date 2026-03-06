af1c63914f18b069d8c05935ba2853a3af53bc52
HaiDangBin <dangsam0944@gmail.com>
2026-03-06 18:56:14 +0700

    demo1

---
+++ js/data.js
@@ -0,0 +1,29 @@
+const products = [
+    { 
+        id: 1, 
+        name: "VinFast Klara S", 
+        price: 35000000, 
+        img: "https://images.unsplash.com/photo-1558981403-c5f9104ad306?q=80&w=500", 
+        battery: "Lithium-ion",
+        range: "120km",
+        desc: "Xe máy điện thông minh với thiết kế Ý sang trọng, kháng nước chuẩn IP67." 
+    },
+    { 
+        id: 2, 
+        name: "Pega Zinger 9", 
+        price: 12500000, 
+        img: "https://images.unsplash.com/photo-1597404294360-feeefa04fc34?q=80&w=500", 
+        battery: "Ắc quy",
+        range: "80km",
+        desc: "Dòng xe quốc dân, bền bỉ, tiết kiệm điện năng tối đa cho học sinh sinh viên." 
+    },
+    { 
+        id: 3, 
+        name: "Asama EBK", 
+        price: 9800000, 
+        img: "https://images.unsplash.com/photo-1485965120184-e220f721d03e?q=80&w=500", 
+        battery: "Ắc quy 48V",
+        range: "50km",
+        desc: "Thiết kế xe đạp truyền thống kết hợp động cơ điện hỗ trợ lực đạp nhẹ nhàng." 
+    }
+];
