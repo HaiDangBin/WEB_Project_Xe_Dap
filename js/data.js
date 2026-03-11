3e5ee68adba3deb04bf8c1bb0dcd2b02a054ddf7
HaiDangBin <dangsam0944@gmail.com>
2026-03-11 19:18:09 +0700

    update img

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const products = [
         id: 1, 
         name: "VinFast Klara S", 
         price: 35000000, 
-        img: "https://images.unsplash.com/photo-1558981403-c5f9104ad306?q=80&w=500", 
+        img: "assets/img/xe1.webp",
         battery: "Lithium-ion",
         range: "120km",
         desc: "Xe máy điện thông minh với thiết kế Ý sang trọng, kháng nước chuẩn IP67." 
@@ -12,7 +12,7 @@ const products = [
         id: 2, 
         name: "Pega Zinger 9", 
         price: 12500000, 
-        img: "https://images.unsplash.com/photo-1597404294360-feeefa04fc34?q=80&w=500", 
+        img: "assets/img/xe12.webp", 
         battery: "Ắc quy",
         range: "80km",
         desc: "Dòng xe quốc dân, bền bỉ, tiết kiệm điện năng tối đa cho học sinh sinh viên." 
@@ -21,9 +21,45 @@ const products = [
         id: 3, 
         name: "Asama EBK", 
         price: 9800000, 
-        img: "https://images.unsplash.com/photo-1485965120184-e220f721d03e?q=80&w=500", 
+        img: "assets/img/xe5.webp", 
         battery: "Ắc quy 48V",
         range: "50km",
+        desc: "Thiết kế xe đạp truyền thống kết hợp động cơ điện hỗ trợ lực đạp nhẹ nhàng." 
+    },
+    { 
+        id: 4, 
+        name: "Bluera X16", 
+        price: 700000, 
+        img: "assets/img/xe6.webp", 
+        battery: "Ắc quy 48V",
+        range: "100km",
+        desc: "Thiết kế xe đạp truyền thống kết hợp động cơ điện hỗ trợ lực đạp nhẹ nhàng." 
+    },
+    { 
+        id: 5, 
+        name: "Takumi Neo", 
+        price: 12000000, 
+        img: "assets/img/xe20.webp", 
+        battery: "Ắc quy 48V",
+        range: "120km",
+        desc: "Thiết kế xe đạp truyền thống kết hợp động cơ điện hỗ trợ lực đạp nhẹ nhàng." 
+    },
+    { 
+        id: 6, 
+        name: "AIWA TAIWAN", 
+        price: 9100000, 
+        img: "assets/img/xe19.webp", 
+        battery: "Ắc quy 48V",
+        range: "200km",
+        desc: "Thiết kế xe đạp truyền thống kết hợp động cơ điện hỗ trợ lực đạp nhẹ nhàng." 
+    },
+    { 
+        id: 7, 
+        name: "Nishiki", 
+        price: 800000, 
+        img: "assets/img/xe18.webp", 
+        battery: "Ắc quy 48V",
+        range: "170km",
         desc: "Thiết kế xe đạp truyền thống kết hợp động cơ điện hỗ trợ lực đạp nhẹ nhàng." 
     }
 ];
